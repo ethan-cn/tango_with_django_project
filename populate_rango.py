edd2985715bb2509071b390db5e82393546ecc9d
ethan-cn <2612869d@student.gla.ac.uk>
2022-02-03 21:02:20 +0000

    Chenyang D_jpg_src
    Signed-off-by: ethan-cn <2612869d@student.gla.ac.uk>

--- populate_rango.py
+++ populate_rango.py
@@ -76,7 +76,7 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name, views=0, likes=0):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
 
     c.views = views
