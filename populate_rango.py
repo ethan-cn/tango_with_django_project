b42c598ee6b6d2cfbfa5227c69c80d1883fc0343
ethan-cn <2612869d@student.gla.ac.uk>
2022-02-04 08:48:54 +0000

    Chenyang D_Chapter6 edit script/re-edit jpg/Add title
    Signed-off-by: ethan-cn <2612869d@student.gla.ac.uk>

--- populate_rango.py
+++ populate_rango.py
@@ -56,7 +56,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added.
 
